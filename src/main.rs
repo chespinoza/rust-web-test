73679a5b80107982b861433c947fcdd6509f3508
Christian Espinoza <chespinoza@gmail.com>
2020-12-13 11:57:42 +0000

    fix: missing semicolon

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder}
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -26,5 +26,4 @@ async fn main()->std::io::Result<()> {
     .bind("127.0.0.1:8080")?
     .run()
     .await
-    
 }
